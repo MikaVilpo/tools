fb0fd904d38b10b03548f073362e455174d264ff
Mika Vilpo <44055222+MikaVilpo@users.noreply.github.com>
2023-03-23 16:15:52 +0200

    Added PredictionSource selector

--- MachineSetup/Microsoft.PowerShell_profile.ps1
+++ MachineSetup/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 #oh-my-posh --init --shell pwsh --config 'ohmyposhv3-v2.json' | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows

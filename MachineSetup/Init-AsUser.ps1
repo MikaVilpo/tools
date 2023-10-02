1f79b8584e3be20612af00cd643fd6bcf4ce6087
Mika Vilpo <44055222+MikaVilpo@users.noreply.github.com>
2023-10-02 14:55:11 +0300

    Update Init-AsUser.ps1

--- MachineSetup/Init-AsUser.ps1
+++ MachineSetup/Init-AsUser.ps1
@@ -10,6 +10,7 @@ winget upgrade --all
 winget install --id=Microsoft.Edge.Beta -e
 
 ## User
+winget install --id=Microsoft.PowerShell -e --scope user
 winget install --id=AgileBits.1Password -e --scope user
 #Change to MSSTORE version
 #winget install --id=WhatsApp.WhatsApp -e --scope user
@@ -21,33 +22,34 @@ winget install --id=Microsoft.AzureDataStudio -e --scope user
 winget install --id=Microsoft.PowerBI -e --scope user
 
 # Dev
-## Admin
-winget install --id=Git.Git -e --accept-package-agreements --accept-source-agreements --scope user
 
 ## User
-winget install --id=GitHub.cli -e --scope user
+winget install --id=Git.Git -e --accept-package-agreements --accept-source-agreements --scope user
 winget install --id=Microsoft.VisualStudioCode -e --scope user
 winget install --id=JanDeDobbeleer.OhMyPosh -e --scope user
-winget install --id=Microsoft.PowerShell -e --scope user
 winget install --id=Microsoft.Bicep -e --scope user
 winget install --id=Postman.Postman -e --scope user
 
-winget install --id=Microsoft.Azure.FunctionsCoreTools -e --scope user
-winget install --id=Microsoft.AzureCLI -e --scope user
-winget install --id=WiresharkFoundation.Wireshark -e --scope user
-
 # PowerShell Modules
-
+Install-Module Az -Scope CurrentUser
+Install-Module Az.Beta -Scope CurrentUser
 
 ## Admin
-oh-my-posh font install
+# winget install --id=GitHub.cli -e
+# winget install --id=Microsoft.Azure.FunctionsCoreTools -e
+winget install --id=Microsoft.AzureCLI -e 
+# oh-my-posh font install
 
 # If needed
-winget install --id=LogiPresentation -e --scope user
-winget install --id=Yubico.YubikeyManager -e --scope user
+#winget install --id=LogiPresentation -e --scope user
 
-winget install --id=Elgato.ControlCenter -e --scope user
-winget install --id=Elgato.StreamDeck -e --scope user
+## User
 winget install --id=Spotify.Spotify -e --scope user
 winget install --id=OBSProject.OBSStudio -e --scope user
 winget install --id=NewTek.NDI5Tools -e --scope user
+
+## Admin
+winget install --id=WiresharkFoundation.Wireshark -e 
+winget install --id=Yubico.YubikeyManager -e 
+winget install --id=Elgato.ControlCenter -e 
+winget install --id=Elgato.StreamDeck -e
